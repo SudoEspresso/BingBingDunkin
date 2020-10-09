4f119b95b72caaacb4b197e42e245c8e7c11cd10
claireputer <26028993+claireputer@users.noreply.github.com>
2020-10-09 15:24:24 -0400

    added completion of the daily set for each account

--- BingBingDunkin.py
+++ BingBingDunkin.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 
 endpoints = ["news/search", "videos/search", "images/search", "shop", "search"]  # no leading /
 distribution = [.2, .05, .1, .05, .6]  # Probability distribution for the above endpoints
-GECKO_DRIVER = 'geckodriver.exe'  # CHANGE ME (path to downloaded web-driver)
+GECKO_DRIVER = 'geckodriver'  # CHANGE ME (path to downloaded web-driver)
 
 """
  ____  __  __ _   ___    ____  __  __ _   ___    ____  _  _  __ _  __ _  __  __ _   _  
@@ -23,7 +23,15 @@ GECKO_DRIVER = 'geckodriver.exe'  # CHANGE ME (path to downloaded web-driver)
 BingBingDunkin' is a program which automates getting search points for multiple accounts.
 """
 
-
+ASCII_ART = """
+ ____  __  __ _   ___    ____  __  __ _   ___    ____  _  _  __ _  __ _  __  __ _   _  
+(  _ \(  )(  ( \ / __)  (  _ \(  )(  ( \ / __)  (    \/ )( \(  ( \(  / )(  )(  ( \ (/     ( (
+ ) _ ( )( /    /( (_ \   ) _ ( )( /    /( (_ \   ) D () \/ (/    / )  (  )( /    /         ) )
+(____/(__)\_)__) \___/  (____/(__)\_)__) \___/  (____/\____/\_)__)(__\_)(__)\_)__)       ........
+                                                                                         |      |]
+                                                                                         \      /
+                                                                                          `----'
+"""
 def wait_for(sec=2, jitter=True, min=3, max=100):
     """
     A function which is provided a minimum amount of time and then adds a random amount
@@ -181,6 +189,167 @@ def start(all_trending_topics: list, user_agent: str, NUM_WORDS: int, mobile=Fal
         print()
 
 
+def output_points(user_agent):
+    print("\n\n\n")
+    print("POINTS REPORT")
+    config = configparser.ConfigParser()
+    config.read("credentials.ini")
+    accounts = {}
+    #  Create a dict of email:pass
+    for key in config['DEFAULT']:
+        if 'email' in key:
+            accounts[config['DEFAULT'][key]] = config['DEFAULT']['password' + key.split("email")[1]]
+
+    for email in accounts.keys():
+        #  Set up the driver profile
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("general.useragent.override", user_agent)  # Sets user agent
+        profile.set_preference("dom.disable_beforeunload", True)  # Disables firefox popups
+        try:
+            driver = webdriver.Firefox(firefox_profile=profile, executable_path=getcwd() + "/{}".format(GECKO_DRIVER))
+            driver.set_page_load_timeout(15)  # Sets a timeout at 15 seconds
+        except Exception as e:
+            print('\nERROR:', e)
+            exit(1)
+
+        password = accounts[email]
+
+        driver.get("https://login.live.com/")
+        wait_for(5, jitter=False)
+        elem = driver.find_element_by_name('loginfmt')
+        elem.clear()
+        elem.send_keys(email)
+        elem.send_keys(Keys.RETURN)
+        wait_for(4, jitter=False)
+        elem1 = driver.find_element_by_name('passwd')
+        elem1.clear()
+        elem1.send_keys(str(password))
+        elem1.send_keys(Keys.ENTER)
+        wait_for(9, jitter=False)
+
+        driver.get("https://www.bing.com/")
+        points = driver.find_element_by_id("id_rc")
+        wait_for(9, jitter=False)
+        points.click()
+        wait_for(9, jitter=False)
+        points.click()
+        wait_for(9, jitter=False)
+        points.click()
+        wait_for(9, jitter=False)
+        print(email)
+        points_num = int(points.text)
+        print("\tTotal points: ", points_num)
+        if points_num >= 6500:
+            print("Time to cash in $$$")
+        wait_for(60, jitter=False)
+
+def daily_set(user_agent):
+    config = configparser.ConfigParser()
+    config.read("credentials.ini")
+    accounts = {}
+    #  Create a dict of email:pass
+    for key in config['DEFAULT']:
+        if 'email' in key:
+            accounts[config['DEFAULT'][key]] = config['DEFAULT']['password' + key.split("email")[1]]
+
+
+    for email in accounts.keys():
+        print("Daily set for ", email)
+        #  Set up the driver profile
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("general.useragent.override", user_agent)  # Sets user agent
+        profile.set_preference("dom.disable_beforeunload", True)  # Disables firefox popups
+        try:
+            driver = webdriver.Firefox(firefox_profile=profile, executable_path=getcwd() + "/{}".format(GECKO_DRIVER))
+            driver.set_page_load_timeout(15)  # Sets a timeout at 15 seconds
+        except Exception as e:
+            print('\nERROR:', e)
+            exit(1)
+
+        # daily set 1
+        driver.get("https://www.bing.com/")
+        points = driver.find_element_by_id("id_rc")
+        wait_for(9, jitter=False)
+        points.click()
+        wait_for(9, jitter=False)
+        driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+        elem = driver.find_element_by_xpath("/html/body")
+        flyout = elem.find_element_by_id("modern-flyout")
+        free_ten_points = flyout.find_element_by_class_name("promo_cont")
+        free_ten_points.click()
+        wait_for(9, jitter=False)
+
+        # complete the bing quiz
+        count = 0
+        driver.switch_to.default_content()
+        wait_for(9, jitter=False)
+        driver.get("https://www.bing.com/")
+        wait_for(9, jitter=False)
+        points = driver.find_element_by_id("id_rc")
+        points.click()
+        wait_for(9, jitter=False)
+        driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+        elem = driver.find_element_by_xpath("/html/body")
+        flyout = elem.find_element_by_id("modern-flyout")
+        quiz = flyout.find_elements_by_class_name("promo_cont")[1]
+        wait_for(9, jitter=False)
+        quiz.click()
+        driver.switch_to.default_content()
+        wait_for(9, jitter=False)
+        for i in range(0,10):
+            question = "QuestionPane" + str(i)
+            answer = "AnswerPane" + str(i)
+
+            content= driver.find_element_by_id("b_content")
+            olist = content.find_element_by_id("b_results")
+            canvas = olist.find_element_by_id("wkCanvas")
+            qa = canvas.find_element_by_id("ListOfQuestionAndAnswerPanes")
+            q1 = qa.find_element_by_id(question)
+            a = q1.find_element_by_class_name("wk_Circle")
+            a.click()
+            wait_for(9, jitter=False)
+
+            content= driver.find_element_by_id("b_content")
+            olist = content.find_element_by_id("b_results")
+            canvas = olist.find_element_by_id("wkCanvas")
+            qa = canvas.find_element_by_id("ListOfQuestionAndAnswerPanes")
+            a1 = qa.find_element_by_id(answer)
+            next = a1.find_element_by_class_name("wk_buttons")
+            button = next.find_element_by_class_name("wk_button")
+            button.click()
+            wait_for(9, jitter=False)
+
+
+        # complete the poll
+        driver.switch_to.default_content()
+        wait_for(9, jitter=False)
+        driver.get("https://www.bing.com/")
+        wait_for(9, jitter=False)
+        points = driver.find_element_by_id("id_rc")
+        points.click()
+        wait_for(9, jitter=False)
+        driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+        elem = driver.find_element_by_xpath("/html/body")
+        flyout = elem.find_element_by_id("modern-flyout")
+        poll = flyout.find_elements_by_class_name("promo_cont")[2]
+        poll.click()
+        driver.switch_to.default_content()
+        wait_for(9, jitter=False)
+        trivia_overlay = driver.find_element_by_id("b_TriviaOverlay")
+        wrapper= trivia_overlay.find_element_by_id("overlayWrapper")
+        button_overlay = wrapper.find_element_by_id("btOverlay")
+        overlay_panel = button_overlay.find_element_by_id("overlayPanel")
+        trivia_overlay_data = overlay_panel.find_element_by_class_name("TriviaOverlayData")
+        poll_overlay = trivia_overlay_data.find_element_by_id("btPollOverlay")
+        poll = poll_overlay.find_element_by_class_name("bt_poll")
+        options = poll.find_element_by_css_selector(".btOptions2.bt_pollOptions")
+        choice = options.find_element_by_id("btoption0")
+        choice.click()
+        wait_for(9, jitter=False)
+        driver.close()
+
+
+
 if __name__ == '__main__':
     #  Don't change. This user agent represents an edge browser which will give you 600 more points per month
     DESKTOP_USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.68"
@@ -196,6 +365,8 @@ if __name__ == '__main__':
         print('\nERROR: {} NOT FOUND'.format(GECKO_DRIVER))
         exit(1)
 
+    print(ASCII_ART)
+
     print("Generating Trending Topics")
     all_trending_topics = google_trends()
 
@@ -207,3 +378,13 @@ if __name__ == '__main__':
 
     print("Starting Mobile\n")
     start(all_trending_topics, MOBILE_USERAGENT, NUM_WORDS_MOBILE, mobile=True)
+
+    print("Finished Mobile")
+    wait_for(60, jitter=False)
+
+    print("Running Daily Set")
+    daily_set(DESKTOP_USERAGENT)
+
+
+    print("Getting Points")
+    output_points(DESKTOP_USERAGENT)
